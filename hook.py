78568735b3b3502849f81016a3ba0479041fe628
Alex <43142871+khyberspache@users.noreply.github.com>
2020-04-17 11:16:20 -0400

    enabled function hooking of abilities (#1)

--- hook.py
+++ hook.py
@@ -11,3 +11,7 @@ async def enable(services):
     BaseWorld.apply_config('build', BaseWorld.strip_yml('plugins/builder/conf/environments.yml')[0])
     build_svc = BuildService(services)
     await build_svc.stage_enabled_dockers()
+
+
+async def expansion(services):
+    await services.get('build_svc').initialize_code_hook_functions()
