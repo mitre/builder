fec6ce300d198bf06f6414d9b2fff28b8ac3d70c
Anna Lin <alin@mitre.org>
2021-05-27 13:46:24 -0400

    Added GUI for builder to display list of enabled environments from yml file

--- hook.py
+++ hook.py
@@ -1,16 +1,23 @@
 from plugins.builder.app.build_svc import BuildService
 from app.utility.base_world import BaseWorld
+from plugins.builder.app.builder_gui import BuilderGUI
 
 name = 'builder'
 description = 'Dynamically compile ability code via docker containers'
-address = None
+address = '/plugin/builder/gui'
 access = BaseWorld.Access.RED
 
 
 async def enable(services):
+    environments = BaseWorld.strip_yml('plugins/builder/conf/environments.yml')
     BaseWorld.apply_config('build', BaseWorld.strip_yml('plugins/builder/conf/environments.yml')[0])
     build_svc = BuildService(services)
     await build_svc.stage_enabled_dockers()
+
+    envs = environments[0]['enabled']
+    builder_gui = BuilderGUI(services, name, description, envs)
+    app = services.get('app_svc').application
+    app.router.add_route('GET', '/plugin/builder/gui', builder_gui.splash)
 
 
 async def expansion(services):
